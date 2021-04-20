99d21f9f801f49f34e917e4af91aa853d058b725
William Ng <william.ng@simonmarkets.com>
2021-04-19 20:59:06 -0400

    Update code

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
-	apiv1 "k8s.io/api/core/v1"
+	//"gopkg.in/yaml.v2"
 
+	corev1 "k8s.io/api/core/v1"
+
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
 
 	//
 	// Uncomment to load all auth plugins
@@ -25,13 +30,36 @@ import (
 	"github.com/mwlng/k8s_resources_sync/pkg/k8s_resources"
 )
 
+var (
+	eksPaths map[string]string = map[string]string{
+		"alpha": "/home/ssm-user/backup/eks/dev/alphaeks/app-services",
+		"qa":    "/home/ssm-user/backup/eks/qa/qaeks/app-services",
+		"prod":  "/home/ssm-user/backup/eks/prod/prodeks/app-services",
+	}
+)
+
+func init() {
+	klog.InitFlags(nil)
+}
+
 func main() {
+	defer func() {
+		klog.Flush()
+	}()
+
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
+
+	environ := flag.String("e", "alpha", "Target environment")
+	deploymentFlag := flag.Bool("deployment", false, "Sync k8s deployment resources")
+	serviceFlag := flag.Bool("service", false, "Sync k8s service resources")
+	cronFlag := flag.Bool("cronjob", false, "Sync k8s cron job resources")
+
+	flag.Set("v", "2")
 	flag.Parse()
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -39,9 +67,39 @@ func main() {
 		panic(err)
 	}
 
+	klog.Infof("Starting to sync k8s resources in %s ...", *environ)
+	if *deploymentFlag {
+		klog.Infof("Syncing k8s deployments resources to %s ...", environ)
+		syncDeployments(environ, config)
+	} else if *serviceFlag {
+		klog.Infof("Syncing k8s services to %s ...", environ)
+		syncServices(environ, config)
+	} else if *cronFlag {
+		klog.Infof("Syncing k8s cron jobs to %s ...", environ)
+		syncCronJobs(environ, config)
+	} else {
+		klog.Infoln("No specified k8s resources to sync, exit !")
+		Usage()
+	}
+}
+
+func Usage() {
+	fmt.Printf("Usage of %s:\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func syncDeployments(environ *string, kubeConfig *rest.Config) {
 	// List Deployments
-	fmt.Printf("Listing deployments in namespace %q:\n", apiv1.NamespaceDefault)
-	deployment, err := k8s_resources.NewDeployment(config, apiv1.NamespaceDefault)
+	fmt.Printf("Listing deployments in namespace %q:\n", corev1.NamespaceDefault)
+	deployment, err := k8s_resources.NewDeployment(kubeConfig, corev1.NamespaceDefault)
 	if err != nil {
 		panic(err)
 	}
@@ -52,8 +110,24 @@ func main() {
 	}
 
 	for _, d := range list.Items {
-		fmt.Printf(" * %s (%d replicas)\n", d.Name, *d.Spec.Replicas)
+		app_name := strings.TrimSpace(strings.Split(d.Name, "-")[0])
+		deployment_manifest := fmt.Sprintf("%s/%s/deployement-%s.yaml", eksPaths[*environ], app_name, app_name)
+		if fileExists(deployment_manifest) {
+			for _, c := range d.Spec.Template.Spec.Containers {
+				fmt.Printf(" * %s (image: %s)\n", d.Name, c.Image)
+			}
+		} else {
+			fmt.Printf("Can't find deployement manifest file: %s \n", deployment_manifest)
+		}
 	}
+}
+
+func syncServices(environ *string, kubeConfig *rest.Config) {
+
+}
+
+func syncCronJobs(environ *string, kubeConfig *rest.Config) {
+
 }
 
 func int32Ptr(i int32) *int32 { return &i }
